9d249e7e127b3684525b951cee57654daee3acfe
Damien Baubriau <dam.baubriau@gmail.com>
2024-09-24 08:26:15 +0100

    Final update

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,20 +1,17 @@
 class QuestionsController < ApplicationController
   def ask
   end
+
   def answer
     @question = params[:question]
-    @answer = coach_response(@question)
-  end
-end
-
-private
-
-def coach_response(message)
-  if message == "I am going to work"
-    "Great!"
-  elsif message.end_with?("?")
-    "Silly question, get dressed and go to work!"
-  else
-    "I don't care, get dressed and go to work!"
+    if @question.blank?
+      @answer = "I can't hear you!"
+    elsif @question =~ /i am going to work/i
+      @answer = "Great!"
+    elsif @question.ends_with?("?")
+      @answer = "Silly question, get dressed and go to work!"
+    else
+      @answer = "I don't care, get dressed and go to work!"
+    end
   end
 end
